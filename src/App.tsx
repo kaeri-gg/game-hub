bc32e9abbfb8b736f6b4f31eae355ca132ab1d1d
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-06-17 18:29:03 +0800

    created page title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import NavBar from "./component/NavBar";
 import { useState } from "react";
 import Genre from "./component/Genre";
+import PageTitle from "./component/PageTitle";
 
 function App() {
+  const [pageTitle, setPageTitle] = useState("Games");
   const [genre, setGenre] = useState([
-    { id: 1, img: "https://picsum.photos/32/", name: "Action" },
-    { id: 2, img: "https://picsum.photos/32/", name: "Indie" },
-    { id: 3, img: "https://picsum.photos/32/", name: "Adventure" },
-    { id: 4, img: "https://picsum.photos/32/", name: "RPG" },
-    { id: 5, img: "https://picsum.photos/32/", name: "Strategy" },
-    { id: 6, img: "https://picsum.photos/32/", name: "Shooter" },
+    { id: 1, img: "https://picsum.photos/32/", name: "Action", link: "#" },
+    { id: 2, img: "https://picsum.photos/32/", name: "Indie", link: "#" },
+    { id: 3, img: "https://picsum.photos/32/", name: "Adventure", link: "#" },
+    { id: 4, img: "https://picsum.photos/32/", name: "RPG", link: "#" },
+    { id: 5, img: "https://picsum.photos/32/", name: "Strategy", link: "#" },
+    { id: 6, img: "https://picsum.photos/32/", name: "Shooter", link: "#" },
   ]);
+
+  const visibleGenre = (name: string) => {
+    setPageTitle(name);
+  };
 
   return (
     <>
       <div className="col-12">
         <NavBar></NavBar>
       </div>
-      <div className="container-fluid">
+      <div className="container-fluid d-flex">
         <div className="col-2">
-          <Genre genres={genre}></Genre>
+          <Genre genres={genre} onClick={(name) => visibleGenre(name)}></Genre>
         </div>
-        <div className="col-9"></div>
+        <div className="col-10">
+          <PageTitle name={pageTitle}></PageTitle>
+        </div>
       </div>
     </>
   );
 }
-
 export default App;
