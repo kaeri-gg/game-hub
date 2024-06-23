efbb85ff7298d8d33398248025183139cd85a89f
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-06-23 18:54:12 +0800

    platform icons

--- src/component/PlatformIcons.tsx
+++ src/component/PlatformIcons.tsx
@@ -9,9 +9,11 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 
 import { Game } from "../hook/useGame";
+import { IconType } from "react-icons";
+import { Icon } from "@chakra-ui/react";
 
 const PlatformIcons = ({ game }: { game: Game }) => {
-  const iconMap = {
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
@@ -22,13 +24,14 @@ const PlatformIcons = ({ game }: { game: Game }) => {
   };
 
   return (
-    <>
+    <div className="d-flex">
       {game.platform_parent.map((platform) => (
-        <p className="card-text" key={platform.id}>
-          {platform.platform}
+        <p className="card-text col-1" key={platform.id}>
+          {/* {platform.platform} */}
+          <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
         </p>
       ))}
-    </>
+    </div>
   );
 };
 
