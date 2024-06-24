577b18e51dd800e63e295b3968d4d6846b9b9001
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-06-25 01:48:33 +0800

    skeleton feature

--- src/component/Skeletons.tsx
+++ src/component/Skeletons.tsx
@@ -3,12 +3,14 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 const Skeletons = () => {
   return (
     <>
-      <Card width="300px">
-        <Skeleton height="200px" />
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Card>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xx-2">
+        <Card width="300px" m={5}>
+          <Skeleton height="200px" />
+          <CardBody>
+            <SkeletonText />
+          </CardBody>
+        </Card>
+      </div>
     </>
   );
 };
